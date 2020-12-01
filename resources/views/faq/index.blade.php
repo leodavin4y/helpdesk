856f800a162e90f087c6043d21fabccd04bae6c3
Artem <fural33t@gmail.com>
2020-12-01 17:41:13 +0500

    Доделал вывод статей из базы на странице FAQ

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -3,8 +3,20 @@
 @section('title', 'База знаний | ' . $_ENV['APP_NAME'])
 
 @section('content')
-    <div class="container">
-        <h1 class="h3 py-3 text-center">Здесь представлены типичные ошибки.</h1>
+
+        <div class="container">
+            <h1 class="h3 py-3 text-center">База знаний</h1>
+        </div>
+    @foreach($allFaqs as $faq)
+        <div  class="alert alert-info">
+            <h3>{{ $faq->title }}</h3>
+            <p>{{ $faq->text }}</p>
+            <p><small>{{ $faq->created_at }}</small></p>
+            <a href="#"><button class="btn btn-info">Детальнее</button></a>
+        </div>
+
+
+        @endforeach
 
 
 @endsection
